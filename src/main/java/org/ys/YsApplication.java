a2bc5b85e93dae81cbcb9c82a718f557bf148254
杨顺 <811570083@qq.com>
2025-07-14 22:21:01 +0800

    feat: 初始化 YS 项目- 新增 application.properties 文件，设置应用名称为 ys - 创建 YsApplication 类作为 Spring Boot 应用的入口 - 添加简单的单元测试类 YsApplicationTests

---
+++ src/main/java/org/ys/YsApplication.java
@@ -0,0 +1,13 @@
+package org.ys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ys/YsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
